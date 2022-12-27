172ab7e631e754002e8a1ab025b56cee6a8c2c87
Achille kouassi <patriceachillekouassin@gmail.com>
2022-12-28 00:31:12 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+typeof(15)
+// Prediction:number
+// Actual:number
+
+typeof(5.5)
+// Prediction: float
+// Actual:number
+
+
+typeof(NaN)
+// Prediction: string
+// Actual: string
+
+
+
+typeof("hello")
+// Prediction: string
+// Actual: string
+
+typeof(true)
+// Prediction: booleen
+// Actual:booleen
+
+typeof(1 != 2)
+// Prediction: true
+// Actual: true
+
+
+"hamburger" + "s"
+// Prediction: hamburgers
+// Actual: hamburgers
+
+"hamburgers" - "s"
+// Prediction:not a number
+// Actual:NaN
+
+
+"1" + "3"
+// Prediction: 13
+// Actual: 13
+
+"1" - "3"
+// Prediction:NaN
+// Actual:NaN
+
+
+"johnny" + 5
+// Prediction: johnn5
+// Actual:string5
+
+
+"johnny" - 5
+// Prediction:NaN
+// Actual:NaN
+
+
+99 * "hello"
+// Prediction: not a number
+// Actual:NaN
+
+
+1 != 1
+// Prediction: false
+// Actual: true
+
+
+1 == "1"
+// Prediction:false
+// Actual:false
+
+
+1 === "1"
+// Prediction:true
+// Actual:false
